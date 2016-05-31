8f608905587c8396c3ce3bb9876931bd3b551c6f
youngindia0123 <anu.anupam14@gmail.com>
2016-05-31 17:50:12 +0530

    Added date picker

--- app/src/main/java/com/youngindia/jobportal/ui/fragment_emp_regitin.java
+++ app/src/main/java/com/youngindia/jobportal/ui/fragment_emp_regitin.java
@@ -1,6 +1,7 @@
 package com.youngindia.jobportal.ui;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,19 +9,25 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.Button;
 
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import android.widget.Toast;
 
 import com.youngindia.jobportal.R;
+import com.youngindia.jobportal.adapter.DateTimePicker;
+
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -39,6 +46,8 @@ public class fragment_emp_regitin extends Fragment {
     Button nxtbtn;
     Spinner spinner_industry;
     TextView edt_other;
+
+    TextView dateResult, date2;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -82,7 +91,8 @@ public class fragment_emp_regitin extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootview=inflater.inflate(R.layout.fragment_fragment_emp_regitin, container, false);
-
+        dateResult = (TextView)rootview.findViewById(R.id.textResult);
+        date2 = (TextView)rootview.findViewById(R.id.textView1);
 
 
         spinner_industry=(Spinner)rootview.findViewById(R.id.spinner_Industry);
@@ -138,6 +148,70 @@ public class fragment_emp_regitin extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
+
+
+    public void customDatePicker_click(View v){
+        // Create the dialog
+        final Dialog mDateTimeDialog = new Dialog(getActivity());
+        // Inflate the root layout
+        final RelativeLayout mDateTimeDialogView = (RelativeLayout) getLayoutInflater().inflate(R.layout.date_time_dialog, null);
+        // Grab widget instance
+        final DateTimePicker mDateTimePicker = (DateTimePicker) mDateTimeDialogView.findViewById(R.id.DateTimePicker);
+        mDateTimePicker.setDateChangedListener((DateTimePicker.DateWatcher) getActivity());
+        mDateTimePicker.initData();
+        //end
+        Button setDateBtn = (Button)mDateTimeDialogView.findViewById(R.id.SetDateTime);
+        // Update demo TextViews when the "OK" button is clicked
+        setDateBtn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                mDateTimePicker.clearFocus();
+	/*				String result_string = mDateTimePicker.getMonth() + "/" + String.valueOf(mDateTimePicker.getDay()) + "/" + String.valueOf(mDateTimePicker.getYear())
+							+ "  " + String.valueOf(mDateTimePicker.getHour()) + ":" + String.valueOf(mDateTimePicker.getMinute());*/
+                String result_string = mDateTimePicker.getMonth() + "/" + String.valueOf(mDateTimePicker.getDay()) + "/" + String.valueOf(mDateTimePicker.getYear());
+
+//					if(mDateTimePicker.getHour() > 12) result_string = result_string + "PM";
+//					else result_string = result_string + "AM";
+                date2.setText(result_string);
+                mDateTimeDialog.dismiss();
+            }
+        });
+
+        Button cancelBtn = (Button)mDateTimeDialogView.findViewById(R.id.CancelDialog);
+        // Cancel the dialog when the "Cancel" button is clicked
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                mDateTimePicker.reset();
+                mDateTimeDialog.cancel();
+            }
+        });
+
+        // Reset Date and Time pickers when the "Reset" button is clicked
+
+        Button resetBtn = (Button)mDateTimeDialogView.findViewById(R.id.ResetDateTime);
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                mDateTimePicker.reset();
+            }
+        });
+
+        // Setup TimePicker
+        // No title on the dialog window
+        mDateTimeDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // Set the dialog content view
+        mDateTimeDialog.setContentView(mDateTimeDialogView);
+        // Display the dialog
+        mDateTimeDialog.show();
+    }
+
+    public void onDateChanged(Calendar c) {
+        Log.e("","" + c.get(Calendar.MONTH) + " " + c.get(Calendar.DAY_OF_MONTH)+ " " + c.get(Calendar.YEAR));
+        String result_string =String.valueOf(c.get(Calendar.MONTH)+1)  + "/" + String.valueOf(c.get(Calendar.DAY_OF_MONTH)) + "/" + String.valueOf(c.get(Calendar.YEAR));
+//			_tvUserDOB.setTypeface(CGlobalVariables.tfFoEnglish);
+        dateResult.setText(result_string+" (mm/dd/yyyy)");
+    }
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
